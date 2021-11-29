2265ee2135becc0a1d21763c013e94c8f52a28a3
EmanSza <eszabadka@hotmail.ca>
2021-11-29 11:32:45 -0500

    Fixed some issues

--- source/events/buttonListener.js
+++ source/events/buttonListener.js
@@ -1,11 +1,11 @@
-const { timeExemptCheck, timeExemptMessage, timeExemptMessage} = require('../utils/liisteners/utils')
+const { timeExemptCheck, timeExemptMessage} = require('../utils/liisteners/utils')
 module.exports = (client, instance) => {
     // Create a new button listener
     client.on("interactionCreate", (interaction) =>  {
         if(!interaction.isButton()) return;
         console.log(`[ButtonListener] Created button interaction: ${interaction.id}`);
-         let timeCheck = timeExemptCheck();
-         let timeMessage = timeExemptMessage()
+         let timeCheck = timeExemptCheck(interaction);
+         let timeMessage = timeExemptMessage(interaction)
         if(!timeCheck) return;
         if(timeMessage) return;
     });
--- source/utils/liisteners/utils.js
+++ source/utils/liisteners/utils.js
@@ -1,23 +1,18 @@
 const { buttonURL } = require('../../../config/WOK.json');
-function timeExemptCheck(interaction) {
-    //  if a button is older then 15 and it is not expemted, delete it
-    const interactionCustomURL = interaction.customId; 
-    console.log(interactionCustomURL);
-     // take buttonURL.timeExemptURL and add it to the interactionCustomURL
-    let timeExempt = false;
-    timeTillDelete = 15;
+
+function timeExemptMessage(interactionn) {
+     let timeTillDelete = 15;
+     let timeExempt = false;
     for(var i = 0; i > buttonURL.timeExemptURL.length; i++) {
         if(buttonURL.timeExemptURL[i] == interaction.customId) timeExempt = true;
-        if(buttonURL.timeExemptURL[i] == interaction.customId) break;
+        if(timeExempt) break;
     }
-    return timeExempt;
-}
-function timeExemptMessage(interaction) {
+
     let returnValue = false;
 // Get minutes since interaction was created
 let minutes = Math.round(Date.now() - interaction.message.createdTimestamp) / 60000;
 
-if(minutes > timeTillDelete && interaction.customId == buttonURL.timeExemptURL[i]) {
+if(minutes > timeTillDelete && !timeExempt) {
     // Delete the orginal message and make it emphialized
     interaction.message.delete();
     let newInteration = interaction.followUp({
