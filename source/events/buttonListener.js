c6016fd7f87aed54f9cfbcbc88bff0be763afb0f
EmanSza <eszabadka@hotmail.ca>
2021-11-30 09:32:19 -0500

    Fixed up branch

---
+++ source/events/buttonListener.js
@@ -0,0 +1,17 @@
+const { timeExemptCheck } = require('../utils/liisteners/utils')
+module.exports = (client, instance) => {
+    client.on("interactionCreate", (interaction) =>  {
+        if(!interaction.isButton()) return;
+        console.log(`[ButtonListener] Created button interaction: ${interaction.id}`);
+         let timeCheck = timeExemptCheck(interaction);
+        if(timeCheck) return;
+        // Add Functions/Button Code here and listen for customId
+
+    });
+}
+
+// Config Export
+module.exports.config = {
+    displayName: 'Button Listener',
+    dbName: 'buttonListener'
+}
