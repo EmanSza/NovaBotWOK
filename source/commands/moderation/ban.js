37e5f30fb4af7cc8e0088f338905f392b64896a1
EmanSza <eszabadka@hotmail.ca>
2021-12-01 13:57:11 -0500

    Updated Ban.js
    Added more safe guards for server protection

--- source/commands/moderation/ban.js
+++ source/commands/moderation/ban.js
@@ -27,17 +27,27 @@ module.exports = {
         // getMember returns GuildMember
         const user = interaction.options.getMember('user')
         const reason = interaction.options.getString('reason')
+         
+        // See if bot has permissions to ban
         if(!user.guild.me.hasPermission('BAN_MEMBERS')) {
             return interaction.reply({
                 content: 'I cannot ban user ' + user.user.tag + ' because I do not have the permission to ban members',
                 ephemeral: true,
              })
             }
+            
+            // See if bot can ban user
+        if(!user.bannable) {
+            interaction.reply({
+                content: 'I cannot ban user ' + user.user.tag + ' because they are higher than me in the role hierarchy',
+                ephemeral: true,
+            })
+        }    
         
         // See if the user can be banned or not
        if(interaction.user.roles.highest.comparePositionTo(user.roles.highest <= 0)) {
               return interaction.reply({
-                content: 'User' + user.user.tag + ' has a equal/higher role than me',
+                content: 'User' + user.user.tag + ' has a equal/higher role then you, so you cannot ban them',
                 ephemeral: true,
               })
        }
@@ -60,3 +70,8 @@ module.exports = {
        // End of Callback
     }
 }
+/*
+TODO:
+    - Add a reason log for the ban
+    - 
+*/ 
